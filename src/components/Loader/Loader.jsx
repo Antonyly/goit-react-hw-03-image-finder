085ccf743b8cbcaa423ad90597c7335eb2ae3013
Antonyly <antonlyzun@gmail.com>
2022-09-27 13:35:13 +0300

    +

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,22 @@
-import { Watch } from 'react-loader-spinner';
-import s from './loader.module.css';
+// import { Watch } from 'react-loader-spinner';
+// import s from './loader.module.css';
 
+// const Loader = () => {
+//   return (
+//     <div className={s.loader}>
+//      <Watch color="#242424" height={100} width={100} />
+//     </div>
+//   );
+// };
+
+
+import { InfinitySpin } from 'react-loader-spinner';
+import css from './loader.module.css';
 const Loader = () => {
   return (
-    <div className={s.loader}>
-     <Watch color="#242424" height={100} width={100} />
+    <div className={css.loader}>
+      <InfinitySpin width="200" color="#4fa94d" />
     </div>
   );
 };
-
 export default Loader;
